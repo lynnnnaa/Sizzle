08b80c93220fe6d8824805c7b968d8be75ce25d5
christina1024 <christinali1024@gmail.com>
2020-03-06 12:41:27 -0700

    all of it. start

---
+++ WpfMid-Prototype/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfMid_Prototype
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+
+        }
+
+        private void menuButton_MouseMove(object sender, MouseEventArgs e)
+        {
+            menuPanel.Visibility = System.Windows.Visibility.Visible;
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        private void menuPanel_MouseLeave(object sender, MouseEventArgs e)
+        {
+            menuPanel.Visibility = System.Windows.Visibility.Hidden;
+        }
+
+        private void menuButton_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            menuPanel.Visibility = System.Windows.Visibility.Visible;
+        }
+    }
+}
